8ee171f04784cfcd6e37f1a0cebd1797d67cb1e0
StognienkoVladislav <stognienkovv@gmail.com>
2018-07-08 18:35:12 +0300

    templates

---
+++ examples/js/vue_template.js
@@ -0,0 +1,9 @@
+
+var vm = new Vue({
+   el : '#vue_det',
+   data: {
+       firstname : "Ria",
+       lastname : "Singh",
+       htmlcontent : "<div><h1>Vue Js Tempalte</h1></div>"
+   }
+});
---
+++ examples/js/vue_template1.js
@@ -0,0 +1,10 @@
+
+var vm = new Vue({
+   el : '#vue_det',
+   data: {
+       firstname : "Ria",
+       lastname : "Singh",
+       htmlcontent : "<div><h1>Vue Js Tempalte</h1></div>",
+       imgsrc : "images/image.jpg"
+   }
+});
